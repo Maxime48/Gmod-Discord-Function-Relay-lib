45d3fc6e05e0abc269c59c2105656f4b9183fa50
Maxime48 <37154360+Maxime48@users.noreply.github.com>
2020-04-27 00:44:46 +0200

    Update api.php
    Updated api to work again

--- Discord Relay System/lua/api.php
+++ Discord Relay System/lua/api.php
@@ -6,24 +6,28 @@
     $webhook_url = $_POST["webhook_url"];
     $json = $_POST["json"];
 
-    if ($pass && $pass == $target_pass)
-    {
-        $ch = curl_init();
-        curl_setopt_array($ch, [
-            CURLOPT_URL => $webhook_url,
-            CURLOPT_POST => true,
-            CURLOPT_POSTFIELDS => $json,
-            CURLOPT_HTTPHEADER => [
-                "Length" => strlen($json),
-                "Content-Type" => "application/json"
-            ]
-        ]);
+     if ($pass && $pass == $target_pass)
+    { 
 
-        echo curl_exec($ch);
-        curl_close($ch);
-    }
+// Replace the URL with your own webhook url
+
+$ch = curl_init();
+
+curl_setopt_array( $ch, [
+    CURLOPT_URL => $webhook_url,
+    CURLOPT_POST => true,
+    CURLOPT_POSTFIELDS => $json,
+    CURLOPT_HTTPHEADER => [
+        "Content-Type: application/json"
+    ]
+]);
+
+$response = curl_exec( $ch );
+curl_close( $ch );
+
+     }
     else
     {
         echo "blocked messages go away";
-    }
+    } 
 ?>
